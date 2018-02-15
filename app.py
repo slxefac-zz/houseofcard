61e4ac73e8683b5aa6f60d39121f4e3ecdbe7fda
fernando <slxefac@gmail.com>
2018-02-15 12:15:13 -0600

    Iniciando la base de datos

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
 
 app = Flask(__name__)
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+class Blogpost(db.Model):
+    __tablename__='post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_poster = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 @app.route('/')
 def index():
