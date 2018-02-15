7869dc567f765491225963e3e11b39bd9fd706e7
fernando <slxefac@gmail.com>
2018-02-15 14:55:10 -0600

    trabajando con la base de datos para parecerse a django

--- app.py
+++ app.py
@@ -4,17 +4,10 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 heroku = Heroku(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ifxjznlzzqneza:1a0abda54f0546ff89ca46aa061f5057d6555fe6c8f9bacd1316dce39251de6c@ec2-107-22-175-33.compute-1.amazonaws.com:5432/d7o4ha9lf0e6n0'
 db = SQLAlchemy(app)
 
-class Blogpost(db.Model):
-    __tablename__='post'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    subtitle = db.Column(db.String(50))
-    author = db.Column(db.String(20))
-    date_poster = db.Column(db.DateTime)
-    content = db.Column(db.Text)
+from models import Blogpost
 
 @app.route('/')
 def index():
