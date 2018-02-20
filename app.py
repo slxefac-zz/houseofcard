5f041f7938ece57a457d0280649038af2ca90ab6
fernando <slxefac@gmail.com>
2018-02-20 10:34:13 -0600

    terminando el tutorial con la escritura a la base de datos

--- app.py
+++ app.py
@@ -23,15 +23,17 @@ class Blogpost(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template('index.html',posts=posts)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Blogpost.query.filter_by(id=post_id).one()
+    return render_template('post.html', post=post)
 
 @app.route('/contact')
 def contact():
