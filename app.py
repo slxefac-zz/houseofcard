5be4b5247957cc3dd4fe4c9aa0957751a6c445cc
fernando <slxefac@gmail.com>
2018-02-22 09:27:07 -0600

    dividiendo la aplicación en modulos

--- app.py
+++ app.py
@@ -4,56 +4,11 @@ from flask_heroku import Heroku
 from datetime import datetime
 
 app = Flask(__name__)
+
+app.config.from_pyfile('config.py')
 heroku = Heroku(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ifxjznlzzqneza:1a0abda54f0546ff89ca46aa061f5057d6555fe6c8f9bacd1316dce39251de6c@ec2-107-22-175-33.compute-1.amazonaws.com:5432/d7o4ha9lf0e6n0'
 db = SQLAlchemy(app)
-
-class Blogpost(db.Model):
-    __tablename__='post'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    subtitle = db.Column(db.String(50))
-    author = db.Column(db.String(20))
-    date_posted = db.Column(db.DateTime)
-    content = db.Column(db.Text)
-
-    def __repr__(self):
-        return '<title %r>' % (self.title)
-
-@app.route('/')
-def index():
-    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
-    return render_template('index.html',posts=posts)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    post = Blogpost.query.filter_by(id=post_id).one()
-    return render_template('post.html', post=post)
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/add')
-def add():
-    return render_template('add.html')
-
-@app.route('/addpost', methods=['POST'])
-def addpost():
-    titulo = request.form['titulo']
-    subtitulo = request.form['subtitulo']
-    autor = request.form['autor']
-    contenido = request.form['contenido']
-
-    post = Blogpost(title=titulo, subtitle=subtitulo, author=autor, content=contenido, date_posted = datetime.now())
-    db.session.add(post)
-    db.session.commit()
-    return redirect(url_for('index'))
+from views import *
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
